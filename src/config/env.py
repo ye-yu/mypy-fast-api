07e71654c62e3c1a1c25278bba2cdaacbedbce89
ye-yu <rafolwen98@gmail.com>
2023-11-16 16:37:00 +0800

    Hide input in ENV errors

--- src/config/env.py
+++ src/config/env.py
@@ -1,10 +1,12 @@
 import os
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, ConfigDict, Field
 
 
 class Env(BaseModel):
     APP_NAME: str = Field(default="")
     APP_SECRET: str
+
+    model_config = ConfigDict(hide_input_in_errors=True)
 
     def __init__(self):
         BaseModel.__init__(self, **os.environ)
