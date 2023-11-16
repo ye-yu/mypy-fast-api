942824d2c8a0d93d564575f53c9283bc32ab3313
ye-yu <rafolwen98@gmail.com>
2023-11-16 16:39:48 +0800

    Remove unused service

--- src/main.py
+++ src/main.py
@@ -1,16 +1,3 @@
-from typing import Union
-
-from fastapi import Depends, FastAPI
-from .main_service import GetResponse, MainService
+from fastapi import FastAPI
 
 app = FastAPI()
-
-
-@app.get("/")
-def read_root(service: MainService = Depends(MainService.instantiate)) -> GetResponse:
-    return service.GetResponse()
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
--- src/test_main.py
+++ src/test_main.py
@@ -3,9 +3,3 @@ from fastapi.testclient import TestClient
 from .main import app
 
 client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"Hello": "World"}
